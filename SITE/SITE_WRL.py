662f9ce20d64d75bc55cc30e04036b62f81005b1
JuliaRosa03 <juliarosacelante2003@gmail.com>
2024-06-26 15:34:09 -0300

    Alteração 26.06

--- SITE/SITE_WRL.py
+++ SITE/SITE_WRL.py
@@ -65,7 +65,7 @@ for table in tables:
     rows = cursor.fetchall()
     dfs[table_name] = pd.DataFrame(rows, columns=[i[0] for i in cursor.description])
 
-table_names = table_names[1:]
+table_names = table_names[0:-1]
 
 conn.close()
 
@@ -125,6 +125,7 @@ if selected_tables:
             st.sidebar.warning("Selecione no máximo uma opção de ID")
     
         #print('df6: ', df6)
+        
 # {=======================Logos IFES e fuso horário=========================}
 st.sidebar.image(imagem_LOGOS, width=250) 
 
@@ -165,9 +166,20 @@ if id and selected_tables:
     st.markdown(f"# Gráfico de desgaste - Análise com todos os diâmetros\n # ID: {', '.join(id)}")
 
     filtered_df = df3[df3["ID"].isin(id)] # Gráficos gerados a partir do id
-    #print('filtered: ', filtered_df)
-    # Selecionar as colunas desejadas
+    print('filtered: ', filtered_df)
 
+    #{=======================Seleção de Datas=========================}
+    col1, col2 = st.columns((2))
+    startDate = pd.to_datetime(filtered_df["DATA"]).min()
+    endDate = pd.to_datetime(filtered_df["DATA"]).max()
+    
+    with col1:
+        date1 = pd.to_datetime(st.date_input("Data de início", startDate))
+
+    with col2:
+        date2 = pd.to_datetime(st.date_input("Data final", endDate))
+
+    # Selecionar as colunas desejadas
     if not filtered_df.empty:
         # Transformar o DataFrame para o formato longo
         long_df = pd.melt(filtered_df, id_vars=['ID','VIDA'], 
@@ -194,7 +206,7 @@ if id and selected_tables:
         filtro_vida = filtered_df[filtered_df['VIDA'] == vida]
 
         if not filtro_vida.empty:
-            # Pegar o primeiro valor encontrado na coluna 'Região A' correspondente
+            # Pegar o primeiro valor encontrado na coluna 'VIDA' correspondente
             registro = filtro_vida['ARQUIVO'].values[0]
             data = filtro_vida['DATA'].values[0]
             hora = filtro_vida['HORA'].values[0]
