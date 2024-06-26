d7bebb14f4512b76eb42414a1e6c334b55153af1
JuliaRosa03 <juliarosacelante2003@gmail.com>
2024-06-26 15:45:13 -0300

    wawa26_06

--- INSPECAO_1_WRL_Copia.py
+++ INSPECAO_1_WRL_Copia.py
@@ -4,10 +4,9 @@ from tkinter import messagebox
 import colorama as color
 import sqlite3 as sql
 import FUNCOES_WRL as fun
+
 from INSPECAO_2_WRL import aba_camera
-
 from direction import direction
-
 caminho = direction()
 
 def tabela(filtro_id=None):
@@ -28,21 +27,16 @@ def ENTRY_INT(inp_text):
     
     return 0 <= value <= 100000000000 #Qual a vida máxima geralmente?
 
-def ENTRY_STRING(inp_text):
-    return all(char.isalpha() or char.isspace() for char in inp_text) or inp_text == ""
-
 def validador(input):
     comando = (input.register(ENTRY_INT), "%P") 
     return comando
 
+def ENTRY_STRING(inp_text):
+    return all(char.isalpha() or char.isspace() for char in inp_text) or inp_text == ""
+
 def voltar(aba_1, aba_2):
     aba_1.deiconify()  # Exiba a proxima janela 
     aba_2.destroy()  # Destrua a janela atual
-    
-def comandos_botao_continuar(inp_janela,inp_usina_grupo, inp_site, inp_furos_ID, inp_tipo, inp_vida, inp_nome,inp_menu):
-    dados = adquirir_dados(inp_usina_grupo, inp_site, inp_furos_ID, inp_tipo, inp_vida, inp_nome)
-    janela_cadastro = aba_camera(inp_janela, dados, inp_menu)
-    janela_cadastro.deiconify()
     
 def adquirir_dados(inp_usina_grupo, inp_site, inp_furos_ID, inp_tipo, inp_vida, inp_nome): #juliaaaaaa
     DADOS_INSERIDOS = []
@@ -82,6 +76,11 @@ def botao_continuar_foto(inp_furos_ID, inp_tipo, inp_vida, inp_nome):
     dados_banco = cursor.fetchall()
     fun.DESCONECTA_BD(conn)
 
+def comandos_botao_continuar(inp_janela,inp_usina_grupo, inp_site, inp_furos_ID, inp_tipo, inp_vida, inp_nome,inp_menu):
+    dados = adquirir_dados(inp_usina_grupo, inp_site, inp_furos_ID, inp_tipo, inp_vida, inp_nome)
+    janela_cadastro = aba_camera(inp_janela, dados, inp_menu)
+    janela_cadastro.deiconify()
+
 def OnDoubleClick(event, listaCli,usina, site, BOF, ID, Furos, Tipo):
     usina.delete(0, tk.END)
     site.delete(0, tk.END)
@@ -98,8 +97,7 @@ def OnDoubleClick(event, listaCli,usina, site, BOF, ID, Furos, Tipo):
         ID.insert(tk.END, col6)
         Furos.insert(tk.END, col4)
         Tipo.insert(tk.END, col5)
-            
-            
+                        
 def tela(inp_janela):
     inp_janela.title("INICIAR INSPECÇÃO")
     inp_janela.configure(background= '#9BCD9B')
@@ -124,94 +122,92 @@ def componentes_frame1(inp_frame,inp_janela):# #TOPLEVEL, inp_menu
     label_usina = fun.CRIAR_LABEL(inp_frame, "Usina/Grupo: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
     label_usina.place(relx=0.05, rely=0.15)
 
-    input_usina = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_usina.place(relx=0.05, rely=0.2, relwidth=0.35, relheight=0.06)
+    input_usina = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_usina.place(relx=0.05, rely=0.2, relwidth=0.3, relheight=0.06)
 
     # {=======================SITE=========================}
     label_site = fun.CRIAR_LABEL(inp_frame, "Site: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
     label_site.place(relx=0.05, rely=0.3)
 
-    input_site = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_site.place(relx=0.05, rely=0.35, relwidth=0.35, relheight=0.06)
+    input_site = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_site.place(relx=0.05, rely=0.35, relwidth=0.3, relheight=0.06)
 
     # {======================= BOF =========================}
     label_BOF = fun.CRIAR_LABEL(inp_frame, "BOF: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
     label_BOF.place(relx=0.05, rely=0.45)
 
-    input_BOF = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_BOF.place(relx=0.05, rely=0.5, relwidth=0.35, relheight=0.06)
+    input_BOF = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_BOF.place(relx=0.05, rely=0.5, relwidth=0.3, relheight=0.06)
     
     # {======================= ID =========================}
     label_ID = fun.CRIAR_LABEL(inp_frame, "ID: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
     label_ID.place(relx=0.05, rely=0.6)
 
-    input_ID = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_ID.place(relx=0.05, rely=0.65, relwidth=0.15, relheight=0.06)
+    input_ID = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_ID.place(relx=0.05, rely=0.65, relwidth=0.13, relheight=0.06)
     
     # {======================= FUROS =========================}
     label_Furos = fun.CRIAR_LABEL(inp_frame, "Furos: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
-    label_Furos.place(relx=0.25, rely=0.6)
+    label_Furos.place(relx=0.2, rely=0.6)
 
-    input_Furos = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_Furos.place(relx=0.25, rely=0.65, relwidth=0.15, relheight=0.06)
+    input_Furos = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_Furos.place(relx=0.2, rely=0.65, relwidth=0.13, relheight=0.06)
 
     # {=======================TIPO=========================}
     label_tipo = fun.CRIAR_LABEL(inp_frame, "Tipo: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
     label_tipo.place(relx=0.05, rely=0.75)
 
-    input_tipo = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_tipo.place(relx=0.05, rely=0.8, relwidth=0.15, relheight=0.06)
+    input_tipo = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_tipo.place(relx=0.05, rely=0.8, relwidth=0.13, relheight=0.06)
     
     # {=======================VIDA=========================}
     label_vida = fun.CRIAR_LABEL(inp_frame, "Vida: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
-    label_vida.place(relx=0.25, rely=0.75)
+    label_vida.place(relx=0.2, rely=0.75)
 
-    input_vida = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand= validador(inp_frame) )
-    input_vida.place(relx=0.25, rely=0.8, relwidth=0.15, relheight=0.06)
+    input_vida = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand= validador(inp_frame) )
+    input_vida.place(relx=0.2, rely=0.8, relwidth=0.13, relheight=0.06)
 
     # {=======================Divisória=========================}
     label_divisor = fun.CRIAR_LABEL(inp_frame, "", '#9BCD9B', "#1C1C1C", 'arial', '20', 'bold')
-    label_divisor.place(relx=0.42, rely=0.15, relwidth=0.005, relheight=0.71)
+    label_divisor.place(relx=0.37, rely=0.15, relwidth=0.005, relheight=0.71)
     
     # {=======================Usuário=========================}
     label_usuario = fun.CRIAR_LABEL(inp_frame, "Usuário: ", '#B4FF9A', "#1C1C1C", 'arial', '20', 'bold')
-    label_usuario.place(relx=0.45, rely=0.75)
+    label_usuario.place(relx=0.4, rely=0.75)
 
-    input_usuario = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand="key")
-    input_usuario.place(relx=0.45, rely=0.8, relwidth=0.5, relheight=0.06)
+    input_usuario = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand="key")
+    input_usuario.place(relx=0.4, rely=0.8, relwidth=0.55, relheight=0.06)
     
     vcmd = (input_usuario.register(ENTRY_STRING), '%P')
     input_usuario.config(validatecommand=vcmd)
 
     # {=======================Botão Voltar e Continuar=========================}
     bt_voltar = fun.CRIAR_BOTAO(inp_frame, "MENU",'#258D19', 'white',3,'20','',"hand2")# #TOPLEVEL, lambda: voltar( inp_menu, inp_janela)
-    bt_voltar.place(relx=0.05, rely=0.9, relwidth=0.15, relheight=0.06)
+    bt_voltar.place(relx=0.05, rely=0.9, relwidth=0.13, relheight=0.06)
     # inp_janela.withdraw()
 
     bt_continuar = fun.CRIAR_BOTAO(inp_frame, "CONTINUAR",'#258D19', 'white',3,'20','',"hand2")#,lambda: comandos_botao_continuar(inp_janela,Var_Usina,Var_site,Var_IDTipo,Var_tipo,input_vida,input_usuario,inp_menu)
-    bt_continuar.place(relx=0.8, rely=0.9, relwidth=0.15, relheight=0.06)
+    bt_continuar.place(relx=0.8, rely=0.9, relwidth=0.13, relheight=0.06)
     
     # {=======================FECHAR ABA=========================}
     bt_fechar_aba_menu = tk.Button(inp_frame, text="X", command=inp_janela.destroy, bg="red").place(relx=0.96, rely=0.02, relwidth=0.03, relheight=0.04) #AVISO ->tirar esta linha pro tk.tk
 
     # {=======================Tabela=========================}
-    
     #OBS: a vida não pode ser menor do que a anterior
-
     label_aviso = fun.CRIAR_LABEL(inp_frame, "Click 2 vezes sobre \na linha desejada", '#9BCD9B', "white", 'calibri', '18', 'bold')
     label_aviso.place(relx=0.8, rely=0.15)
     
-    filtrar_ID = tk.Entry(inp_frame, validate= "key",font=("Arial", 25), validatecommand= validador(inp_frame) )
-    filtrar_ID.place(relx=0.45, rely=0.2, relwidth=0.1, relheight=0.06)
+    filtrar_ID = tk.Entry(inp_frame, validate= "key",font=("Arial", 20), validatecommand= validador(inp_frame) )
+    filtrar_ID.place(relx=0.4, rely=0.2, relwidth=0.1, relheight=0.06)
 
     bt_buscar = fun.CRIAR_BOTAO(inp_frame, "Buscar ID", '#258D19', 'white', 3, '20', "", "hand2", lambda: buscar_id(filtrar_ID.get()))
-    bt_buscar.place(relx=0.55, rely=0.2, relwidth=0.15, relheight=0.06)
+    bt_buscar.place(relx=0.5, rely=0.2, relwidth=0.13, relheight=0.06)
 
     def buscar_id(id_filtro):
-        if not id_filtro: 
-            Tabela.delete(*Tabela.get_children()) 
+        Tabela.delete(*Tabela.get_children()) 
+        conn, cursor = fun.CONECTA_BD(caminho)
 
-            conn, cursor = fun.CONECTA_BD(caminho)
+        if not id_filtro: 
             comando = "SELECT Grupo, Site, BOF, TIPO, ID, ULTIMA_VIDA FROM DADOS_EMPRESAS"
             cursor.execute(comando)
             dados_tabela = cursor.fetchall()
@@ -221,26 +217,21 @@ def componentes_frame1(inp_frame,inp_janela):# #TOPLEVEL, inp_menu
                 Tabela.insert("", tk.END, values=dado)
         
         else:
-            Tabela.delete(*Tabela.get_children()) 
-
-            conn, cursor = fun.CONECTA_BD(caminho)
             comando = f"SELECT Grupo, Site, BOF, TIPO, ID, ULTIMA_VIDA FROM DADOS_EMPRESAS WHERE ID = ?"
             cursor.execute(comando, (id_filtro,))
             dados_filtrados = cursor.fetchall()
             fun.DESCONECTA_BD(conn)
 
             if not dados_filtrados:
-                messagebox.showwarning("ID Não Encontrado", f"O ID '{id_filtro}' não foi encontrado na base de dados.")
-                Tabela.delete(*Tabela.get_children()) 
+                messagebox.showwarning("ID Não Encontrado", f"O ID '{id_filtro}' não foi encontrado na base de dados.") 
 
-                conn, cursor = fun.CONECTA_BD(caminho)
                 comando = "SELECT Grupo, Site, BOF, TIPO, ID, ULTIMA_VIDA FROM DADOS_EMPRESAS"
                 cursor.execute(comando)
                 dados_tabela = cursor.fetchall()
                 fun.DESCONECTA_BD(conn)
-
                 for dado in dados_tabela:
                     Tabela.insert("", tk.END, values=dado)
+            
             else:
                 for dado in dados_filtrados:
                     Tabela.insert("", tk.END, values=dado)
@@ -252,8 +243,7 @@ def componentes_frame1(inp_frame,inp_janela):# #TOPLEVEL, inp_menu
     style.configure("Treeview.Heading", font=('Verdana', 12,'bold'))
     style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Verdana', 11))
 
-    # Ocultando a primeira coluna
-    Tabela.column("#0", width=0, stretch=tk.NO)
+    Tabela.column("#0", width=0, stretch=tk.NO)# Ocultando a primeira coluna
     Tabela.heading("#0", text="")
 
     Tabela.heading("#1", text="Grupo")
@@ -273,7 +263,7 @@ def componentes_frame1(inp_frame,inp_janela):# #TOPLEVEL, inp_menu
     for dado in tabela():
         Tabela.insert("", tk.END, values=(dado[0], dado[1], dado[2], dado[3], dado[4], dado[5]))
         
-    Tabela.place(relx=0.45, rely=0.29, relwidth=0.5, relheight=0.45)
+    Tabela.place(relx=0.4, rely=0.29, relwidth=0.55, relheight=0.45)
     
     scroolLista = tk.Scrollbar(inp_frame, orient='vertical', command=Tabela.yview)
     Tabela.configure(yscrollcommand = scroolLista.set)
